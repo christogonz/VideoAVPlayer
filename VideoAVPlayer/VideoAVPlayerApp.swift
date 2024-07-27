64fe78e0933142494b404c1514faf46bdc5f7923
Chris Gonzalez <christogonz92@gmail.com>
2024-07-27 14:13:36 +0200

    Initial Commit

---
+++ VideoAVPlayer/VideoAVPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoAVPlayerApp.swift
+//  VideoAVPlayer
+//
+//  Created by Christopher Gonzalez on 2024-07-27.
+//
+
+import SwiftUI
+
+@main
+struct VideoAVPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
